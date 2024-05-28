792a4a1e6da6890e661d8b4f1d5242d67ee06bee
Takuto Susuki <stukt04@gmail.com>
2024-05-29 03:33:15 +0900

    solve practice of "Hello World"

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main(){
+    practice1()
+}
+
+// Complete the code to make the program print "Mary is 20 years old" to standard output
+fun practice1(){
+    val name = "Mary"
+    val age = 20
+    print("$name is $age years old")
 }
