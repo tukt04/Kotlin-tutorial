050f8d16ac1b8f36b2ec243c242ff14789d676a3
Takuto Susuki <stukt04@gmail.com>
2024-05-31 00:50:15 +0900

    solve problem "Collections"

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,8 @@
 
 fun main(){
     // practice1()
-    practice2()
+    // practice2()
+    practice3()
 }
 
 // Complete the code to make the program print "Mary is 20 years old" to standard output
@@ -18,4 +19,24 @@ fun practice2(){
     val d: Long = 100_000_000_000_000
     val e: Boolean = false
     val f: Char = '\n'
+}
+
+fun practice3(){
+    // Exercise1
+    val greenNumbers = listOf(1, 4, 23)
+    val redNumbers = listOf(17, 2)
+
+    val totalCount: Int = greenNumbers.count() + redNumbers.count()
+    println(totalCount)
+
+    // Exercise2
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+    val requested = "smtp"
+    val isSupported = requested.uppercase() in SUPPORTED
+    println("Support for $requested: $isSupported")
+
+    // Exercise3
+    val number2word = mapOf(1 to "one", 2 to "two", 3 to "three")
+    val n = 2
+    println("$n is spelt as '${number2word[n]}'")
 }
