9765537d919f6e667de38505dd90249876e1f1c0
Takuto Susuki <stukt04@gmail.com>
2024-05-31 00:36:42 +0900

    solve problem "Basic types"

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,7 @@
 
 fun main(){
-    practice1()
+    // practice1()
+    practice2()
 }
 
 // Complete the code to make the program print "Mary is 20 years old" to standard output
@@ -8,4 +9,13 @@ fun practice1(){
     val name = "Mary"
     val age = 20
     print("$name is $age years old")
+}
+
+fun practice2(){
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
 }
